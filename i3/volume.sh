bab45f3427566e29f8b204d65531fc71d86f4cd8
Marin Dedvukaj <marin.dedvukaj2005@gmail.com>
2025-02-12 13:15:44 +0100

    changed the files and also edited the requirement

--- i3/volume.sh
+++ i3/volume.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DEFAULT_VOLUME=50
+pulseaudio-ctl set $DEFAULT_VOLUME
+
 VOLUME=$(pulseaudio-ctl full-status | awk '{print $1}')
 MUTE=$(pulseaudio-ctl full-status | awk '{print $2}')
 if [ "$MUTE" == "yes" ]; then
@@ -6,3 +9,4 @@ if [ "$MUTE" == "yes" ]; then
 else
     echo " $VOLUME%"
 fi
+
